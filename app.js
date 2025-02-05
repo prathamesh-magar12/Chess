c396523c37bd9122a3b6aaa3b96fb06b18ab7fab
prathamesh-magar12 <prathamesh.magar12@gmail.com>
2025-02-05 22:41:46 +0530

    refactor: fix comment spacing in app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ io.on("connection", (uniquesocket) => {
   uniquesocket.on("disconnect", () => {
     console.log(`Socket ${uniquesocket.id} disconnected`);
 
-    //Remove disconnected player
+    // Remove disconnected player
     if (uniquesocket.id === players.white) {
       delete players.white;
       console.log("White player disconnected");
@@ -50,7 +50,7 @@ io.on("connection", (uniquesocket) => {
       io.emit("playerLeft", { role: "b" });
     }
 
-    //Notify all clients about the updated player roles
+    // Notify all clients about the updated player roles
     io.emit("updateRoles", players);
   });
 
